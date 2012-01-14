a731d014c582baaa2d345c719e583beb42b6065b
Reuben Thomas <rrt@sc3d.org>
2012-01-14 18:14:26 +0000

    occurs.c: remove some unused includes.

--- occurs.c
+++ occurs.c
@@ -10,12 +10,9 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <errno.h>
 #include <string.h>
-#include <ctype.h>
 #include <locale.h>
-#include <getopt.h>
 #include <regex.h>
 #include "xalloc.h"
 #include "hash.h"
