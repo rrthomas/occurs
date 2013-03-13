23a594366112a7659eed8616d7d6f9488a9f9e87
Reuben Thomas <rrt@sc3d.org>
2013-03-13 20:10:24 +0000

    occurs.sh: work with any locale (except multibyte until tr made to work with those)

--- occurs.sh
+++ occurs.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 # occurs
 # Count number of occurrences of words in a file
-# Reuben Thomas   18/2/98
+# Reuben Thomas   18/2/98, 13/3/13
+
+# Doug McIlroy's solution:
+# tr -cs A-Za-z '\n' | tr A-Z a-z | sort | uniq -c | sort -rn | sed ${1}q 
 
 # Suggestion from Underhanded C contest 2006:
-#tr "[:space:]" "[\n*]" | sort | awk 'length($0)>0' | uniq -c
-tr A-Z a-z < $1 | tr -cs A-Za-z '\n' | grep [[:print:]] | sort | uniq -c | sort
+# tr "[:space:]" "[\n*]" | sort | awk 'length($0)>0' | uniq -c
+
+# Until tr is fixed to work with multibyte encodings, this won't work properly
+tr [:upper:] [:lower:] < $1 | tr -cs [:alpha:] '\n' | grep [[:print:]] | sort | uniq -c | sort
