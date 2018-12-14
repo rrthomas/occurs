cf21aa9a0caf42f7222e01170b2698ce0479b95b
Reuben Thomas <rrt@sc3d.org>
2018-12-14 11:53:05 +0000

    occurs.lua: update to work with latest stdlib

--- occurs.lua
+++ occurs.lua
@@ -1,28 +1,36 @@
 #!/usr/bin/env lua
-prog = {
-  name = "occurs",
-  version = "occurs 0.87 (09 Mar 2013) by Reuben Thomas <rrt@sc3d.org>",
-  purpose = "Count the occurrences of each symbol in a file.",
-  notes = "The default symbol type is words (-s \"([[:alpha:]]+)\"); other useful settings\n" ..
-    "include:\n\n" ..
-    "  non-white-space characters: -s \"[^[:space:]]+\"\n" ..
-    "  alphanumerics and underscores: -s \"[[:alnum:]_]+\"\n" ..
-    "  XML tags: -s \"<([a-zA-Z_:][a-zA-Z_:.0-9-]*)[[:space:]>]\""
-}
+spec = [=[
+occurs 0.90
+Copyright (c) 2018 Reuben Thomas <rrt@sc3d.org>
 
-require "std"
+Usage: occurs [OPTION...] [FILE...]
+
+Count the occurrences of each symbol in input.
+
+The default symbol type is words (-s "([[:alpha:]]+)"); other useful settings
+include:
+
+    non-white-space characters: -s "[^[:space:]]+"
+    alphanumerics and underscores: -s "[[:alnum:]_]+"
+    XML tags: -s "<([a-zA-Z_:][a-zA-Z_:.0-9-]*)[[:space:]>]"
+
+Options:
+
+  -s, --symbol=REGEXP      symbols are given by REGEXP
+  -n, --nocount            don't show the frequencies or total
+      --help               display this help, then exit
+      --version            display version information, then exit
+]=]
+
+local std = require "std"
 rex_posix = require "rex_posix"
-
--- Command-line options
-options = {
-  {{"nocount", "n"}, "don't show the frequencies or total"},
-  {{"symbol", "s"}, "symbols are given by REGEXP", "Req", "REGEXP"},
-}
 
 -- Parse command-line args
 os.setlocale ("")
-getopt.processArgs (prog)
-local symbolPat = getopt.opt.symbol and table.remove (getopt.opt.symbol) or "([[:alpha:]]+)"
+local OptionParser = require "std.optparse"
+local parser = OptionParser (spec)
+_G.arg, opts = parser:parse (_G.arg)
+local symbolPat = opts.symbol or "([[:alpha:]]+)"
 
 -- Compile symbol-matching regexp
 local ok, pattern = pcall (rex_posix.new, symbolPat)
@@ -32,20 +40,19 @@ end
 
 -- Process input
 local freq = {}
-io.processFiles (function (file, number)
-                   for line in io.lines () do
-                     for s in rex_posix.gmatch (line, pattern) do
-                       freq[s] = (freq[s] or 0) + 1
-                     end
-                   end
-               end)
+std.io.process_files (function (file, number)
+                        -- FIXME: make slurp work in pipes
+                        for s in rex_posix.gmatch (std.io.slurp (), pattern) do
+                          freq[s] = (freq[s] or 0) + 1
+                        end
+                      end)
 
 -- Write output
 local symbols = 0
 for s in pairs (freq) do
-  io.writelines (s .. (getopt.opt.nocount and "" or " " .. freq[s]))
+  std.io.writelines (s .. (opts.nocount and "" or " " .. freq[s]))
   symbols = symbols + 1
 end
-if not getopt.opt.nocount then
-  warn ("total symbols: " .. symbols)
+if not opts.nocount then
+  std.io.warn ("total symbols: " .. symbols)
 end
